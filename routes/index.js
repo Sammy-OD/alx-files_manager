23b32f62f940d9c7781c2be26ef60da79c20c6da
Sammy-OD <odunlamisamuel4@gmail.com>
2024-03-14 04:46:23 +0100

    files manager

--- routes/index.js
+++ routes/index.js
@@ -8,52 +8,28 @@ const router = express.Router();
 
 
   // App Controller
-  router.get('/status', (req, res) => {
-    AppController.getStatus(req, res);
-  });
+  router.get('/status', AppController.getStatus);
 
-  router.get('/stats', (req, res) => {
-    AppController.getStats(req, res);
-  });
+  router.get('/stats', AppController.getStats);
 
-  router.post('/users', (req, res) => {
-    UsersController.postNew(req, res);
-  });
+  router.post('/users', UsersController.postNew);
 
-  router.get('/connect', (req, res) => {
-    AuthController.getConnect(req, res);
-  });
+  router.get('/connect', AuthController.getConnect);
 
-  router.get('/disconnect', (req, res) => {
-    AuthController.getDisconnect(req, res);
-  });
+  router.get('/disconnect', AuthController.getDisconnect);
 
-  router.get('/users/me', (req, res) => {
-    UsersController.getMe(req, res);
-  });
+  router.get('/users/me', UsersController.getMe);
 
-  router.post('/files', (req, res) => {
-    FilesController.postUpload(req, res);
-  });
+  router.post('/files', FilesController.postUpload);
 
-  router.get('/files/:id', (req, res) => {
-    FilesController.getShow(req, res);
-  });
+  router.get('/files/:id', FilesController.getShow);
 
-  router.get('/files', (req, res) => {
-    FilesController.getIndex(req, res);
-  });
+  router.get('/files', FilesController.getIndex);
 
-  router.put('/files/:id/publish', (req, res) => {
-    FilesController.putPublish(req, res);
-  });
+  router.put('/files/:id/publish', FilesController.putPublish);
 
-  router.put('/files/:id/unpublish', (req, res) => {
-    FilesController.putUnpublish(req, res);
-  });
+  router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
-  router.post('/files/:id/data', (req, res) => {
-    FilesController.getFile(req, res);
-  });
+  router.post('/files/:id/data', FilesController.getFile);
 
 export default router;
